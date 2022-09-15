745a3d94b5bb39e3d36c589bf3991fd86d0a1e6c
Jihoon2817 <113011623+Jihoon2817@users.noreply.github.com>
2022-09-15 11:04:42 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1 {
+    private int rollno;
+    private string name;
+    private int age;
+
+    public void SetInfo(string name,
